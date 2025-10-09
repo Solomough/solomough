9524e96df4b8c7bbcbf6ee928fa571d2413df8a2
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-10-09 16:08:02 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
 
+export default function Footer(){
+  return (
+    <footer className="mt-12 pb-12 text-center text-sm text-gray-400">
+      <div>© Solomough — Out of Darkness to Light ✝️</div>
+      <div className="italic mt-2">“The entrance of Thy Word giveth light.” — Psalm 119:130</div>
+    </footer>
+  )
+}
